716c809be8cfc3ff1f637362de937fe8ed16f552
Chetan Patil <chetanpatil6303@gmail.com>
2025-03-11 00:53:18 +0530

    update EPMS created employee class in which getter methods are added and also it is an abstract class which includes abstract method

--- EmployeePayrollManagementSystem/EmployeePayrollManagementSystem.java
+++ EmployeePayrollManagementSystem/EmployeePayrollManagementSystem.java
@@ -2,6 +2,37 @@ package EmployeePayrollManagementSystem;
 import java.util.*;
 import java.util.ArrayList;
 
+abstract class Employee {
+    private String name;
+    private int id;
+
+    public Employee(String name,int id){
+        this.id = id;
+        this.name = name;
+    }
+
+    // abstraction getter methods
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    
+    //abstract method
+    abstract double calculateSalary();
+
+    @Override
+    public String toString(){
+        String s = "Employee{" + "name='" + name + '\'' + ", id=" + id + ",salary = \"calculateSalary()+\"}";
+        return s;
+    }
+    
+}
+
 public class EmployeePayrollManagementSystem {
     
 }
