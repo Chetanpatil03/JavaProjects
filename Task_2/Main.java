8acbd4ca83633a09f9e2ff3a8f98e83c5fa0fbee
Chetan Patil <chetanpatil6303@gmail.com>
2025-05-11 22:28:26 +0530

    Create Main.java

---
+++ Task_2/Main.java
@@ -0,0 +1,25 @@
+ class Student {
+    String name;
+    int age;
+    String major;
+
+    public Student(String name, int age, String major) {
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Major: " + major);
+    }
+
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
